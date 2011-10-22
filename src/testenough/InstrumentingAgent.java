a4db9f41e6d13046680a1d80b9ae8f5acc237a5a
Pavan Sudarshan <itspanzi@gmail.com>
2011-10-22 22:38:50 +0530

    Added a BSD 2 license. Also added a pkg task to get the jar and the distributable out. Added the parsing of arguments for config file and lib directory. Defaulted both of them to the same value as available in the dist.

--- src/testenough/InstrumentingAgent.java
+++ src/testenough/InstrumentingAgent.java
@@ -6,25 +6,45 @@ import testenough.weaver.BCWeaver;
 import java.io.File;
 import java.io.IOException;
 import java.lang.instrument.Instrumentation;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.jar.JarFile;
 
 public class InstrumentingAgent {
 
     static final String CONFIG_FILE_PATH = "configFilePath";
     static final String LIB_DIR = "lib";
+    private static final String DEFAULT_CONFIG_PATH = "./config.properties";
+    private static final String DEFAULT_LIB_DIR = "lib";
 
-    public static void agentMain(String argument, Instrumentation instrumentation) throws IOException {
+    public static void premain(String argument, Instrumentation instrumentation) throws IOException {
         Map<String, String> nameToValues = parseArguments(argument);
+        addJarsFromLibToBootstrapperClassPath(instrumentation, nameToValues);
         instrumentation.addTransformer(new CallTrackingTransformer(new Configuration(configContents(nameToValues)), new BCWeaver()));
     }
 
+    private static void addJarsFromLibToBootstrapperClassPath(Instrumentation instrumentation, Map<String, String> nameToValues) throws IOException {
+        String libDir = nameToValues.get(LIB_DIR);
+        if (libDir==null) libDir = DEFAULT_LIB_DIR;
+        Collection<File> files = FileUtils.listFiles(new File(libDir), new String[]{"jar"}, true);
+        for (File jar : files) {
+            instrumentation.appendToBootstrapClassLoaderSearch(new JarFile(jar));
+        }
+    }
+
     private static String configContents(Map<String, String> nameToValues) throws IOException {
-        return FileUtils.readFileToString(new File(nameToValues.get(CONFIG_FILE_PATH)));
+        String pathname = nameToValues.get(CONFIG_FILE_PATH);
+        if (pathname == null) {
+            pathname = DEFAULT_CONFIG_PATH;
+        }
+        File config = new File(pathname);
+        return !config.exists() ? "" : FileUtils.readFileToString(config);
     }
 
     private static Map<String, String> parseArguments(String argument) {
         Map<String, String> nameToValues = new HashMap<String, String>();
+        if (argument.trim().isEmpty()) return nameToValues;
         for (String arg : argument.split("&")) {
             String[] nameAndValue = arg.split(":");
             nameToValues.put(nameAndValue[0], nameAndValue[1]);
--- test/testenough/InstrumentingAgentTest.java
+++ test/testenough/InstrumentingAgentTest.java
@@ -1,23 +1,83 @@
 package testenough;
 
+import org.apache.commons.io.FileUtils;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
 import testenough.weaver.BCWeaver;
 
+import java.io.File;
 import java.io.IOException;
 import java.lang.instrument.Instrumentation;
+import java.util.jar.JarFile;
 
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.*;
 
 public class InstrumentingAgentTest {
 
+
+    private File config;
+    private Instrumentation instrumentation;
+
+    @Before
+    public void setUp() throws Exception {
+        config = new File("config.properties");
+        FileUtils.write(config, configFileContents());
+        FileUtils.forceDeleteOnExit(config);
+        instrumentation = mock(Instrumentation.class);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        verify(instrumentation, atLeastOnce()).appendToBootstrapClassLoaderSearch(any(JarFile.class));
+        verifyNoMoreInteractions(instrumentation);
+        FileUtils.forceDelete(config);
+    }
+
     @Test
     public void shouldAddANewClassTransformer() throws IOException {
-        Instrumentation instrumentation = mock(Instrumentation.class);
-        InstrumentingAgent.agentMain(String.format("%s:resource/config.properties&%s:.", InstrumentingAgent.CONFIG_FILE_PATH, InstrumentingAgent.LIB_DIR), instrumentation);
+        InstrumentingAgent.premain(String.format("%s:resource/config.properties&%s:.", InstrumentingAgent.CONFIG_FILE_PATH, InstrumentingAgent.LIB_DIR), instrumentation);
 
-        Configuration configuration = new Configuration(String.format("%s=tlb.foo", Configuration.POPULATE_INCLUDE_PACKAGES));
+        Configuration configuration = new Configuration(configFileContents());
         verify(instrumentation).addTransformer(new CallTrackingTransformer(configuration, new BCWeaver()));
 
-        verifyNoMoreInteractions(instrumentation);
+    }
+
+    private String configFileContents() {
+        return String.format("%s=tlb.foo", Configuration.POPULATE_INCLUDE_PACKAGES);
+    }
+
+    @Test
+    public void shouldDefaultToConfigFileIfNotSpecified() throws IOException {
+        InstrumentingAgent.premain(String.format("%s:.", InstrumentingAgent.LIB_DIR), instrumentation);
+
+        Configuration configuration = new Configuration(configFileContents());
+        verify(instrumentation).addTransformer(new CallTrackingTransformer(configuration, new BCWeaver()));
+    }
+
+    @Test
+    public void shouldUseAnEmptyConfigurationWhenTheConfigFileIsNotFound() throws IOException {
+        InstrumentingAgent.premain(String.format("%s:foo_bar&%s:.", InstrumentingAgent.CONFIG_FILE_PATH, InstrumentingAgent.LIB_DIR), instrumentation);
+
+        Configuration configuration = new Configuration("");
+        verify(instrumentation).addTransformer(new CallTrackingTransformer(configuration, new BCWeaver()));
+    }
+
+    @Test
+    public void testShouldAddTheJarsInLibDirToBootstrapperClasspath() throws Exception {
+        InstrumentingAgent.premain(String.format("%s:test_folder", InstrumentingAgent.LIB_DIR), instrumentation);
+        Configuration configuration = new Configuration(configFileContents());
+        verify(instrumentation).addTransformer(new CallTrackingTransformer(configuration, new BCWeaver()));
+    }
+
+    @Test
+    public void shouldDefaultTheLibFolder() throws IOException {
+        InstrumentingAgent.premain("", instrumentation);
+        Configuration configuration = new Configuration(configFileContents());
+        verify(instrumentation).addTransformer(new CallTrackingTransformer(configuration, new BCWeaver()));
     }
 }
